596c5b999d3104a85566ae2ab0a3518997380823
Dragon3DGraff <dragon3dgraff@mail.ru>
2020-04-29 23:37:03 +0300

    changed manipulate buutons to radio

--- src/UI/ManipulateToolbar.js
+++ src/UI/ManipulateToolbar.js
@@ -18,26 +18,95 @@ function createManipulateToolbar ( taScene ){
 
 	let manipulatingContainer = ta_UI.createContainer( 'ManipulateToolbar', mainToolbar );
 
-	let buttonSelect = ta_UI.addElement(
-		manipulatingContainer,
-		'button',
-		'Select ',
-		'',
-		function () {
+		// Select Button
+		let radioSelect = document.createElement( 'input' );
+		radioSelect.type = 'radio';
+		radioSelect.name = 'manupulateRadio';
+		radioSelect.id = 'SelectRadio';
+		radioSelect.value = 'Select';
+		radioSelect.checked = true;
+		radioSelect.addEventListener( 'click', switchMode );
+		manipulatingContainer.appendChild( radioSelect );
+
+		let labelSelect = document.createElement( 'label' );
+		labelSelect.innerHTML = 'Select';
+		labelSelect.htmlFor = radioSelect.id;
+		manipulatingContainer.appendChild( labelSelect );
+
+		// Move Button
+		let radioMove = document.createElement( 'input' );
+		radioMove.type = 'radio';
+		radioMove.name = 'manupulateRadio';
+		radioMove.id = 'MoveRadio';
+		radioMove.value = 'Move';
+		radioMove.addEventListener( 'click', switchMode );
+		manipulatingContainer.appendChild( radioMove );
+
+		let labelMove = document.createElement( 'label' );
+		labelMove.innerHTML = 'Move';
+		labelMove.htmlFor = radioMove.id;
+		manipulatingContainer.appendChild( labelMove );
+
+		// Rotate Button
+		let radioRotate = document.createElement( 'input' );
+		radioRotate.type = 'radio';
+		radioRotate.name = 'manupulateRadio';
+		radioRotate.id = 'RotateRadio';
+		radioRotate.value = 'Rotate';
+		radioRotate.addEventListener( 'click', switchMode );
+		manipulatingContainer.appendChild( radioRotate );
+
+		let labelRotate = document.createElement( 'label' );
+		labelRotate.innerHTML = 'Rotate';
+		labelRotate.htmlFor = radioRotate.id;
+		manipulatingContainer.appendChild( labelRotate );
+
+		// Scale Button
+		let radioScale = document.createElement( 'input' );
+		radioScale.type = 'radio';
+		radioScale.name = 'manupulateRadio';
+		radioScale.id = 'ScaleRadio';
+		radioScale.value = 'Scale';
+		radioScale.addEventListener( 'click', switchMode );
+		manipulatingContainer.appendChild( radioScale );
+
+		let labelScale = document.createElement( 'label' );
+		labelScale.innerHTML = 'Scale';
+		labelScale.htmlFor = radioScale.id;
+		manipulatingContainer.appendChild( labelScale );
+
+		// Drag Button
+		let radioDrag = document.createElement( 'input' );
+		radioDrag.type = 'radio';
+		radioDrag.name = 'manupulateRadio';
+		radioDrag.id = 'DragRadio';
+		radioDrag.value = 'Drag';
+		radioDrag.addEventListener( 'click', switchMode );
+		manipulatingContainer.appendChild( radioDrag );
+
+		let labelDrag = document.createElement( 'label' );
+		labelDrag.innerHTML = 'Drag';
+		labelDrag.htmlFor = radioDrag.id;
+		manipulatingContainer.appendChild( labelDrag );
+
+
+function switchMode( selectedRadio ) {
+
+	let selectedButton = selectedRadio.target.id;
+
+	switch (selectedButton) {
+		case 'SelectRadio':
+
 			taScene.mode.action = 'select';
 			taScene.transformControlsMode = '';
 			taScene.transformControls.detach( taScene.currentSelection.object );
 			taScene.dragControls.deactivate();
 			taScene.controls.enableRotate = true;
-		}
+			
+			break;
 
-	);
-	ta_UI.addElement(
-		manipulatingContainer,
-		'button',
-		'Move',
-		"",
-		function () {
+		case 'MoveRadio':
+
 			taScene.transformControlsMode = 'translate';
 			if( taScene.currentSelection.object ) {
 				taScene.transformControls.attach( taScene.currentSelection.object );
@@ -46,15 +115,11 @@ function createManipulateToolbar ( taScene ){
 			taScene.dragControls.deactivate();
 			taScene.mode.action = 'select';
 			taScene.controls.enableRotate = true;
-		}
+		
+			break;
 
-	);
-	ta_UI.addElement(
-		manipulatingContainer,
-		'button',
-		'Rotate',
-		"",
-		function () {
+		case 'RotateRadio':
+
 			taScene.transformControlsMode = 'rotate';
 			if( taScene.currentSelection.object ) {
 				taScene.transformControls.attach( taScene.currentSelection.object );
@@ -63,15 +128,11 @@ function createManipulateToolbar ( taScene ){
 			taScene.dragControls.deactivate();
 			taScene.mode.action = 'select';
 			taScene.controls.enableRotate = true;
-		}
+		
+			break;
 
-	);
-	ta_UI.addElement(
-		manipulatingContainer,
-		'button',
-		'Scale',
-		"",
-		function () {
+		case 'ScaleRadio':
+
 			if( taScene.currentSelection.object ) {
 				taScene.transformControls.attach( taScene.currentSelection.object );
 			}
@@ -81,15 +142,10 @@ function createManipulateToolbar ( taScene ){
 			taScene.dragControls.deactivate();
 			taScene.mode.action = 'select';
 			taScene.controls.enableRotate = true;
-		}
+		
+			break;
 
-	);
-	ta_UI.addElement(
-		manipulatingContainer,
-		'button',
-		'Drag',
-		"",
-		function () {
+		case 'DragRadio':
 
 			if( taScene.currentSelection.object ) {
 
@@ -103,11 +159,14 @@ function createManipulateToolbar ( taScene ){
 			taScene.transformControlsMode = '';
 			taScene.mode.action = '';
 			taScene.dragControls.activate();
-			// taScene.controls.enableRotate = false;
-			// taScene.controls.transformGroup = true;
-		}
 
-	);
+			break;
+	
+		default:
+			break;
+	}
+
+}
 
 	console.log( 'ManipulateToolbar created' );
 
